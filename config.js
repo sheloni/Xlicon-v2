73bd3945431755bef05bb46c86cc0f5dab0b7c7f
sheloni <145893606+sheloni@users.noreply.github.com>
2023-09-24 16:29:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
+global.Owner = ["+919778160974"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["+919778160974"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["+919778160974"];
 global.OwnerName = "Slasher";
 global.BotName = "XLICON-MD";
-global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.packname = "Ｏ Ｂ Ａ Ｎ Ａ Ｉ  _";                             //Do not change.
+global.author = "_";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
 
